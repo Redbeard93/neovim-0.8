6f33b0a5c2cfde13c2aa66836327b35eb948c766
Redbeard93 <67786490+Redbeard93@users.noreply.github.com>
2023-01-26 06:51:41 +0800

    Update go.lua

--- lua/hon6/dap/go.lua
+++ lua/hon6/dap/go.lua
@@ -1,5 +1,4 @@
--- pacman -S delve
--- go install github.com/go-delve/delve/cmd/dlv@latest
+-- if not install delve via mason, do `pacman -S delve` or `go install github.com/go-delve/delve/cmd/dlv@latest`
 --export GOROOT=/usr/lib/go -- go语言安装目录
 --export GOPATH=~/Projects/go -- go语言工作区
 --export GOBIN=$GOPATH/bin -- 存放go语言可执行文件目录
